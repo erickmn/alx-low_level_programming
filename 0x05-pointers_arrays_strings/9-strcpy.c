29309cf353bed09a26ba30938185949b7c0e6e2b
Erick Wanjiru <mef14482@gmail.com>
2023-02-27 11:33:00 -0800

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,17 +8,19 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int l = 0;
 	int x = 0;
 
-	while (*(src + i) != '\0')
+	while (*(src + l) != '\0')
 	{
-		i++;
+		l++;
 	}
-	for (; x < i; x++)
+	for ( ; x < l ; x++)
 	{
 		dest[x] = src[x];
 	}
-	dest[i] = '\n';
+	dest[l] = '\0';
 	return (dest);
 }
+
+
