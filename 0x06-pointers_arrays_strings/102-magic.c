d62eb22b599d1b1fa16005d7d240c0d95dc7a6a7
Erick Wanjiru <mef14482@gmail.com>
2023-03-02 11:47:14 -0800

    it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,7 +2,7 @@
 
 /**
  * main - write your code here
- * Return: Always 0 (Success)
+ *Return: always 0
  */
 
 int main(void)
@@ -13,20 +13,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-
 	/*
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
+	 * - you are not allowed to code anything else
 	 */
-
 	*(p + 5) = 98;
-
-	/* ...so that this prints 98\n */
-
+	/* ... so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
